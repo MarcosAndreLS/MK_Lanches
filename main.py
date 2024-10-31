224d5ea63621807414a1c6eac944b43f69dfa4b8
Marcos André <119796772+MarcosAndreLS@users.noreply.github.com>
2024-10-31 14:22:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import random
 Autenticavel.register(Administradores)
 
 cont = 0
-#Desconto.register(Pedido)
+
 lan = Lanchonete()
 
 
 def menu():
+    print()
     try:
-        op = int(input('-----------MENU-----------\n1 - cadastrar administrador\n2 - cadastrar Atendente\n3 - cadastrar cliente\n4 - realizar um pedido\n5 - consultar um pedido\n6 - mostrar cofre da lanchonete\n7 - listar funcionarios\n0 - sair\n--------------------------\n'))
+        op = int(input('-----------MENU-----------\n1 - cadastrar administrador\n2 - cadastrar Atendente\n3 - cadastrar cliente\n4 - realizar um pedido\n5 - consultar um pedido\n6 - mostrar cofre da lanchonete\n7 - listar funcionarios\n8 - listar clientes\n9 - buscar por cpf\n10 - listar todos os pedidos\n0 - sair\n--------------------------\nEscolha uma opção\n>>> '))
         return op
     except:
         print('Opção digitado não é um inteiro')
 
 
+print('Boas-vindas a Lanchonete "MK Lanches"')
 while True:
     op = menu()
-
+    print()
     if op == 2:
         cpf = str(input('CPF:'))
         print(lan.add_atendente(cpf)[1])
@@ -39,10 +41,14 @@ while True:
     elif op == 5:
         try:
             IdP = int(input('Id do pedido:'))
+            print()
             lan.consultar_pedido(IdP)
+            print()
+            lan._pedido[IdP]._historico.mostrar_historico()
         except:
-            print('Informe um numero inteiro')
+            print('Error')
     elif op == 6:
+        print('Somente administradores podem ter acesso ao cofre da lanchonete')
         cpf = str(input('CPF: '))
         lan.cofre_lanchonete(cpf)
     elif op == 1:
@@ -50,6 +56,12 @@ while True:
         print(lan.add_administradores(cpf)[1])
     elif op == 7:
         lan.exibir_Funcionarios()
+    elif op == 8:
+        lan.exibir_clientes()
+    elif op == 9:
+        lan.buscar_cpf()
+    elif op == 10:
+        lan.exibir_pedidos()
     elif op == 0:
         print('\nSaindo da execução...\n')
         break
